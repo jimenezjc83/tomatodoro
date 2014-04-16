3d3c9962f90b6f4fa6d0bb0842d2b858a01d9766
Juan Carlos Jiménez <juancho@appleboxjc.local>
2014-04-16 16:33:14 -0500

    Initial Commit

---
+++ Tomatodoro/TOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TOAppDelegate.h
+//  Tomatodoro
+//
+//  Created by Juan Carlos Jiménez on 16/04/14.
+//  Copyright (c) 2014 NeblinaSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tomatodoro/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tomatodoro
+//
+//  Created by Juan Carlos Jiménez on 16/04/14.
+//  Copyright (c) 2014 NeblinaSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TOAppDelegate class]));
+    }
+}
